a22d2cebab48118c7020a988a6ecee08f8dc9ade
NeuYash <pawar.ya@northeastern.edu>
2024-08-10 04:46:20 -0400

    Medicine and MedicineAPI implemented

---
+++ pharmease/src/main/java/edu/neu/csye7374/MedicineAPI.java
@@ -0,0 +1,11 @@
+package edu.neu.csye7374;
+
+public interface MedicineAPI {
+	String medicineDescription();
+
+	String medicineManufacturer();
+
+	int noOfMedicinesManufactured();
+
+	double medicinePrice();
+}
