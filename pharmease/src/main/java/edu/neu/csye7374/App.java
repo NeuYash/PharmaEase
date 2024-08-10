a02901e6ff61f3e122858b941ff0692cf5ba85c7
NeuYash <pawar.ya@northeastern.edu>
2024-08-10 02:37:36 -0400

    Project creation, Maven

---
+++ pharmease/src/main/java/edu/neu/csye7374/App.java
@@ -0,0 +1,13 @@
+package edu.neu.csye7374;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
