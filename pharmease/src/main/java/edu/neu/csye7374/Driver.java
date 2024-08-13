1d64a2c155881a056e4002537e6b76399525c38a
Naga Venkatesh Gavini <nagavenkat9948@gmail.com>
2024-08-12 20:43:46 -0400

    Integrated all design patterns

--- pharmease/src/main/java/edu/neu/csye7374/Driver.java
+++ pharmease/src/main/java/edu/neu/csye7374/Driver.java
@@ -1,13 +1,7 @@
 package edu.neu.csye7374;
 
-/**
- * Hello world!
- *
- */
-public class Driver 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Driver {
+    public static void main(String[] args) {
+        Demo.main();
     }
 }
